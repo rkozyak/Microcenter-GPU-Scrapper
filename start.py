f22253a1db21140ad4445c1958f68d4d8e968681
Richard Kozyak <61517945+rkozyak@users.noreply.github.com>
2024-09-16 17:53:23 -0700

    scanned from 660000 to 660000

--- start.py
+++ start.py
@@ -7,8 +7,8 @@ import os
 print('Program started')
 
 # Configurations
-start_id = 660001
-end_id = 660010
+start_id = 660000
+end_id = 680000
 base_url = 'https://www.microcenter.com/product/{}/gpu'
 file_path = 'discovered_gpus.csv'
 non_gpu_file_path = 'discovered_non_gpus.csv'
